14af779df00423501b7c28318cb657b395a96773
itstrH <trannguyenbahuy2908@gmail.com>
2025-05-06 17:35:33 +0700

    updated temp movie card

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ImgTemp from '../assets/temp-1.jpeg'
+
 
 const MovieList = ({title}) => {
   return (
     <div className='bg-black text-white p-10 mb-10'>
         <h2 className='uppercase text-xl mb-4'>{title}</h2>
         <div className="flex items-center space-x-4">
-            <div className="w-[200px] h-[300px] bg-red-500">
-
+            <div className="w-[200px] h-[300px] bg-red-500 relative group">
+                <div className='
+                group-hover:scale-105
+                trasition-transform duration-500
+                ease-in-out
+                w-full h-full cursor-pointer'>
+                <div className="absolute top-0 left-0 w-full h-full bg-black/40"></div>
+                <img 
+                src={ImgTemp} 
+                alt="" 
+                className='w-full h-full object-cover'
+                />
+                <div className="absolute bottom-4 left-2">
+                    <p className="uppercase text-md">Nghe Noi Em Thich Toi</p>
+                </div>   
+                </div>
             </div>
         </div>
     </div>
